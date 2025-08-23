bb8dc422f05e68a172f763fc9e379d393d30fc6a
Harishankar177 <harishankar04v@gmail.com>
2025-08-24 02:48:15 +0530

    align file structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from "react";
+import AppRouter from "./router/AppRouter";
 
-const App = () => {
-  return (
-    <div>
-      hello railways  
-    </div>
-  )
+function App() {
+  return <AppRouter />;
 }
 
-export default App
+export default App;
